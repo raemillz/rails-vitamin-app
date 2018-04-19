632ad0918f0873580d7d441180172d771de3990d
raemillz <raeanne1991@gmail.com>
2018-04-19 19:31:20 -0400

    added benefit nested attributes

--- app/models/vitamin.rb
+++ app/models/vitamin.rb
@@ -5,4 +5,8 @@ class Vitamin < ApplicationRecord
   has_many :users, through: :user_vitamins
   has_many :vitamin_pack_vitamins
   has_many :vitamin_packs, through: :vitamin_pack_vitamins
+
+  validates_presence_of :name
+
+  accepts_nested_attributes_for :benefits, reject_if: proc { |attributes| attributes['name'].blank? }
 end
