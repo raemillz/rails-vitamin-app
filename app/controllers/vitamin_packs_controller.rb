289680c795f39a51acc563268ab771cf1fd96422
raemillz <raeanne1991@gmail.com>
2018-04-19 14:27:07 -0400

    added controller methods

--- app/controllers/vitamin_packs_controller.rb
+++ app/controllers/vitamin_packs_controller.rb
@@ -1,2 +1,70 @@
 class VitaminPacksController < ApplicationController
+
+  def show
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      @vitamin_pack = @user.vitamin_packs.find_by(id: params[:id])
+      if @vitamin_pack.nil?
+        redirect_to user_vitamin_packs_path(@user), alert: "Vitamin pack not found"
+      end
+    else
+      @vitamin_pack = VitaminPack.find(params[:id])
+    end
+  end
+
+  def new
+    if params[:user_id] && !User.exists?(params[:user_id])
+      redirect_to new_user_path, alert: "User not found."
+    else
+      @vitamin_pack = VitaminPack.new(user_id: params[:user_id])
+    end
+  end
+
+  def create
+    @vitamin_pack = VitaminPack.new(vitamin_pack_params)
+    if @vitamin_pack.save
+      redirect_to @vitamin_pack
+    else
+      render :new
+    end
+  end
+
+  def edit
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      if @user.nil?
+        redirect_to new_user_path, alert: "User not found."
+      else
+        @vitamin_pack = @user.vitamin_packs.find_by(id: params[:id])
+        redirect_to user_vitamin_packs_path(@user), alert: "Vitamin Pack not found." if @vitamin_pack.nil?
+      end
+    else
+      @vitamin_pack = VitaminPack.find(params[:id])
+    end
+  end
+
+  def update
+    @vitamin_pack = VitaminPack.find(params[:id])
+
+    @vitamin_pack.update(vitamin_pack_params)
+
+    if @vitamin_pack.save
+      redirect_to @vitamin_pack
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @vitamin_pack = VitaminPack.find(params[:id])
+    @vitamin_pack.destroy
+    flash[:notice] = "Vitamin Pack deleted."
+    redirect_to user_path
+  end
+
+  private
+
+  def vitamin_pack_params
+    params.require(:vitamin_pack).permit(:name, :user_name, :user_id)
+  end
 end
--- app/models/vitamin_pack.rb
+++ app/models/vitamin_pack.rb
@@ -2,4 +2,13 @@ class VitaminPack < ApplicationRecord
   belongs_to :user
   has_many :vitamin_pack_vitamins
   has_many :vitamin_packs, through: :vitamin_pack_vitamins
+
+  def user_name
+    self.try(:user).try(:name)
+  end
+
+  def user_name=(name)
+    user = User.find_or_create_by(name: name)
+    self.user = user
+  end
 end
