2c652a10cf5de1b9934c7baaa740dd0aaa2918f7
raemillz <raeanne1991@gmail.com>
2018-04-20 16:10:41 -0400

    added new formatting

--- app/models/benefit.rb
+++ app/models/benefit.rb
@@ -1,4 +1,7 @@
 class Benefit < ApplicationRecord
   has_many :vitamin_benefits
   has_many :vitamins, through: :vitamin_benefits
+
+  validates_presence_of :name
+  validates :name, uniqueness: true
 end
