d11617fb31f010bcbd09e3eb2addce8409fc2c91
raemillz <raeanne1991@gmail.com>
2018-04-20 13:42:39 -0400

    edited pack form layout

--- app/models/vitamin.rb
+++ app/models/vitamin.rb
@@ -5,8 +5,11 @@ class Vitamin < ApplicationRecord
   has_many :users, through: :user_vitamins
   has_many :vitamin_pack_vitamins
   has_many :vitamin_packs, through: :vitamin_pack_vitamins
-  accepts_nested_attributes_for :benefits
+
   validates_presence_of :name
+
+  accepts_nested_attributes_for :benefits, reject_if: proc { |attributes| attributes['name'].blank? }
+
 
 
 
