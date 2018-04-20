09f6ab110c2f6bf3e03fa707bf6d2214cd58eb1a
raemillz <raeanne1991@gmail.com>
2018-04-20 16:39:16 -0400

    updated UX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
   has_many :vitamin_packs
   has_many :user_vitamins
   has_many :users, through: :user_vitamins
+
+  validates_presence_of :name
+
 end
