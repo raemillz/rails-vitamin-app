999bd68a76f5fb6209100de263159f34558318ae
raemillz <raeanne1991@gmail.com>
2018-04-25 15:48:24 -0400

    added model scope method

--- app/models/vitamin.rb
+++ app/models/vitamin.rb
@@ -1,17 +1,19 @@
 class Vitamin < ApplicationRecord
   has_many :vitamin_benefits
   has_many :benefits, through: :vitamin_benefits
-  has_many :user_vitamins
-  has_many :users, through: :user_vitamins
   has_many :vitamin_pack_vitamins
   has_many :vitamin_packs, through: :vitamin_pack_vitamins
+  has_many :users, through: :vitamin_packs
+
 
   validates_presence_of :name
   validates :name, uniqueness: true
 
   accepts_nested_attributes_for :benefits, reject_if: proc { |attributes| attributes['name'].blank? }
 
-
+  def self.most_popular
+    Vitamin.select("vitamins.*, COUNT(users.id) AS u_count").joins(:users).group("vitamins.id").order("u_count DESC").limit(1)
+  end
 
 
 end
