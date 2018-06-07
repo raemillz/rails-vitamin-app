488299cc0da590aeecec063e65e6e0c2a2a02304
raemillz <raeanne1991@gmail.com>
2018-06-07 12:29:34 -0400

    improved new vitamin rendering

--- app/assets/javascripts/vitamins.js
+++ app/assets/javascripts/vitamins.js
@@ -8,11 +8,14 @@ $(function () {
     posting.done(function(data) {
 
       var vitamin = data;
-      $("#vitaminName").text(vitamin["name"]);
-      $("#vitaminCapsuleAmount").text(vitamin["capsule_amount"]);
-      vitamin["benefits"].forEach(function(data){
-          $("#vitaminBenefits").text(data.id);
-      })
+      $("#vitaminName").text('Vitamin: ' + vitamin['name']);
+      $("#vitaminCapsuleAmount").text('Dosage: ' + vitamin["capsule_amount"]);
+      var benefits = vitamin["benefits"];
+      var benefitList = "";
+      benefits.forEach(function(benefit) {
+        benefitList += '<li class="js-benefit" data-id="' + benefit["id"] + '">' + 'Benefit ID: '+ benefit["id"] + '</li>';
+      });
+      $("#vitaminBenefits").html(benefitList);
     });
   });
 });
