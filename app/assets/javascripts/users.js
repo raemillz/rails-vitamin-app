920251133a2a61930d3e3c9169072e4e5f041562
raemillz <raeanne1991@gmail.com>
2018-06-21 23:15:56 -0400

    created dropdown menu

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,29 @@
-$(document).on ("turbolinks:load", function(){
-  $("a.load_packs").on("click", function(e){
-    $.get(this.href).done(function(response){
-      $("div.packs").html(response)
-    })
-    e.preventDefault();
-  })
-})
+// $(document).on ("turbolinks:load", function(){
+//   $("a.load_packs").on("click", function(e){
+//     $.get(this.href).done(function(response){
+//       $("div.packs").html(response)
+//     })
+//     e.preventDefault();
+//   })
+// })
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
