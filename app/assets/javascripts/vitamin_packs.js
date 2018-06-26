f8c8b36b7b2df09aae28f0b4ca38f392e7f84819
raemillz <raeanne1991@gmail.com>
2018-06-25 20:18:56 -0400

    made syntax changes suggested in assessment

--- app/assets/javascripts/vitamin_packs.js
+++ app/assets/javascripts/vitamin_packs.js
@@ -1,74 +1,83 @@
-var VitaminPack = function(pack) {
+function VitaminPack(pack) {
   this.id = pack.id;
   this.name = pack.name;
   this.user = pack.user;
-  this.user_id = pack.user_id
+  this.userId = pack.user_id
   this.vitamins = pack.vitamins;
   this.vitaminCount = pack.vitamin_count
 };
 
 VitaminPack.prototype.showPageLink = function(text) {
-  let output = '<a href="/user/' + this.user_id + '/vitamin_packs/' + this.id + '" class="js-packs-show" id="pack-' + this.id + '">';
-    output += text;
-  output += '</a>';
+  let output =
+    `<a
+      href="/user/${ this.userId }/vitamin_packs/${this.id}"
+      class="js-packs-show"
+      id="pack-${this.id}">
+      ${text}
+    </a>`;
   return output;
 }
 
 VitaminPack.prototype.editPageLink = function() {
-  let output = '<a class="btn btn-secondary" href="/vitamin_packs/' + this.id + '/edit" class="edit_vitamin_pack">';
-    output += "Edit";
-  output += '</a>';
+  let output =
+  `<a
+    class="btn btn-secondary"
+    href="/vitamin_packs/${this.id}/edit"
+    class="edit_vitamin_pack"> Edit
+  </a>`;
   return output;
 }
 
 VitaminPack.prototype.deleteLink = function() {
-  let output = '<a class="btn btn-danger" data-confirm="Are you sure you really want to delete this vitamin pack?" rel="nofollow" data-method="delete" href="/vitamin_packs/' + this.id + '">';
-    output += "Delete";
-  output += '</a>';
+  let output =
+  `<a
+    class="btn btn-danger"
+    data-confirm="Are you sure you really want to delete this vitamin pack?"
+    rel="nofollow"
+    data-method="delete"
+    href="/vitamin_packs/${this.id}"> Delete
+  </a>`;
   return output;
 }
 
 VitaminPack.prototype.vitaminListLink = function() {
-  let output = '<a href="/vitamin_packs/' + this.id + '/vitamin_list" class="js-packs-vitamin-list">';
-    if ( this.vitaminCount === 1 ) {
-      output += "1 vitamin";
-    } else {
-      output += this.vitaminCount + ' vitamins';
+  let output = `<a href="/vitamin_packs/${this.id}/vitamin_list"
+    class="js-packs-vitamin-list">`;
+      if ( this.vitaminCount === 1 ) {
+        output += "1 vitamin";
+      } else {
+        output += this.vitaminCount + ' vitamins';
     }
   output += '</a>';
   return output;
 }
 
 var buildVitaminPackHeaders = function() {
-  let output = '<h1>';
-    output += 'Vitamin Packs';
-  output += '</h1>';
-  output += '<div class="row align-items-center">';
-    output += '<div class="align-items-center col-lg-6 order-lg-1 hidden-xs">';
-      output += '<h3>Name</h3>';
-    output += '</div>';
-    output += '<div class="align-items-center col-lg-6 order-lg-2 hidden-xs">';
-      output += '<h3>Vitamins</h3>';
-    output += '</div>';
-  output += '</div>';
+  let output =
+  `<h1> Vitamin Packs </h1>
+    <div class="row align-items-center">
+      <div class="align-items-center col-lg-6 order-lg-1 hidden-xs">
+        <h3>Name</h3>
+      </div>
+      <div class="align-items-center col-lg-6 order-lg-2 hidden-xs">
+        <h3>Vitamins</h3>
+      </div>
+    </div>`;
   return output;
 }
 
 VitaminPack.prototype.buildVitaminPackRow = function() {
-  let output = '<div class="pack row center text-center">';
-    output += '<div class="col-lg-6 center text-center">';
-      output += '<h4>';
-        output += this.showPageLink(this.name);
-      output += '</h4>';
-      output += '<p>';
-        output += this.editPageLink();
-        output += this.deleteLink();
-      output += '</p>';
-    output += '</div>';
-    output += '<div class="col-lg-6 center text-center">';
-      output += '<p>' + this.vitaminListLink() + '</p>';
-    output += '</div>';
-  output += '</div>';
+  let output =
+  `<div class="pack row center text-center">
+    <div class="col-lg-6 center text-center">
+      <h4> ${this.showPageLink(this.name)} </h4>
+      <p> ${this.editPageLink()} ${this.deleteLink()} </p>
+    </div>
+    <div class="col-lg-6 center text-center">
+      <p> ${this.vitaminListLink()} </p>
+    </div>
+  </div>`;
+
   return output;
 }
 
@@ -78,39 +87,40 @@ VitaminPack.prototype.buildVitaminPack = function(options) {
     output = '';
   } else {
     output += '<h6 class="f2 normal"><a class="js-packs-index" href="/vitamin_packs"><span style="position:relative; top:-0.1rem">&larr;</span> All Vitamin Packs</a></h6>';
-  }
-  output += '<h1>Vitamin Pack</h1>';
-  output += '<h2>' + this.name + '</h2>';
-  output += '<p>';
-    output += this.editPageLink();
-    output += this.deleteLink();
-  output += '</p>';
-  output += '<h2>Vitamins:</h2>'
-  $.each(this.vitamins, function(index, value){
-    output += '<div class="vitamin">';
-      output += '<h4>';
-        output += '<a href="/vitamins/' + value.id + '">' + value.name + '</a>';
-      output += '</h4>';
-      output += '<a href="/vitamins/' + value.id + 'edit">Edit</a>';
-      output += ', <a data-confirm="Are you sure you want to delete this vitamin?" rel="nofollow" data-method="delete" href="/vitamins/' + value.id + '">Delete</a>';
-    output += '</div>';
-  });
-  return output;
+    }
+    output += `<h1>Vitamin Pack</h1>
+    <h2> ${this.name} </h2>
+    <p> ${this.editPageLink()} ${this.deleteLink()} </p>
+    <h2>Vitamins:</h2>`;
+    $.each(this.vitamins, function(index, value){
+      output +=
+      `<div class="vitamin">
+        <h4> <a href="/vitamins/${value.id}"> ${value.name} </a> </h4>
+        <a href="/vitamins/${value.id}edit"> Edit </a>,
+        <a data-confirm="Are you sure you want to delete this vitamin?" rel="nofollow" data-method="delete" href="/vitamins/${value.id}">Delete</a>
+        </div>`;
+    });
+    return output;
 }
 
 VitaminPack.prototype.buildVitaminList = function() {
-  let output = '<h6 class="f2 normal"><a class="js-packs-index" href="/vitamin_packs"><span style="position:relative; top:-0.1rem">&larr;</span> All Vitamin Packs</a></h6>';
-  output += '<h3>Vitamin Pack</h3>';
-  output += '<h5>';
-    output += '<a href="/vitamin_packs/' + this.id + '" class="js-pack-show" id="pack-' + this.id + '">' + this.name + '</a>';
-  output += '</h5>';
-  output += '<h3>Vitamins</h3>';
-  output += '<div>';
-    $.each(this.vitamins, function(index, value){
-      output += '<p>';
-        output += '<a href="/vitamins/' + value.id + '">' + value.name + '</a>';
-      output += '</p>';
-    });
+  let output =
+  `<h6 class="f2 normal">
+    <a class="js-packs-index" href="/vitamin_packs">
+    <span style="position:relative; top:-0.1rem">&larr;</span>
+    All Vitamin Packs</a>
+  </h6>
+  <h3>Vitamin Pack</h3>
+  <h5>
+  <a href="/vitamin_packs/${this.id}"
+    class="js-pack-show"
+    id="pack-${this.id}">${this.name}</a>
+  </h5>
+  <h3>Vitamins</h3>
+  <div>`;
+  $.each(this.vitamins, function(index, value){
+    output += `<p> <a href="/vitamins/${value.id}"> ${value.name} </a> </p>`;
+  });
   output += '</div>';
   return output;
 }
@@ -163,10 +173,14 @@ $(document).on ("turbolinks:load", function(){
 
 const getVitaminPacks = function (){
   $.get('/vitamin_packs.json').done(function(data){
+    const packs = data.sort(function(a, b){
+      return a.vitamin_count - b.vitamin_count;
+    })
     let vitaminPacks = buildVitaminPackHeaders();
     vitaminPacks += '<div class="packs">';
-      $.each(data, function(index, value){
-        let vitaminPack = new VitaminPack(data[index]);
+      $.each(packs, function(index, value){
+
+        let vitaminPack = new VitaminPack(value);
         vitaminPacks += vitaminPack.buildVitaminPackRow();
       });
     vitaminPacks += '</div>';
--- spec-js.md
+++ spec-js.md
@@ -4,17 +4,17 @@ Specs:
 - Use jQuery for implementing new requirements
 
 - Include a show resource rendered using jQuery and an Active Model Serialization JSON backend.
-  [X]--> For VitaminPacks and Vitamins.
+  [X]--> For VitaminPacks.
 - Include an index resource rendered using jQuery and an Active Model Serialization JSON backend.
-  [X]--> For VitaminPacks and Vitamins.
+  [X]--> For VitaminPacks.
 - Include at least one has_many relationship in information rendered via JSON and appended to the DOM.
   [X]--> User has_many VitaminPacks and they appended to User show page once link is clicked.
 - Use your Rails API and a form to create a resource and render the response without a page refresh.
   [X]--> New VitaminPack form (takes a long time, but works).
 - Translate JSON responses into js model objects.
-  [X]--> In vitamin_packs.js and vitamins.js
+  [X]--> In vitamin_packs.js
 - At least one of the js model objects must have at least one method added by your code to the prototype.
-  [X]--> In vitamin_packs.js and vitamins.js
+  [X]--> In vitamin_packs.js
 
 Confirm
 
